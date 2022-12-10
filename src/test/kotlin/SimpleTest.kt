aff5ae9adfeec358ff0b46d84cc3874b3928c9a6
sergeych <real.sergeych@gmail.com>
2022-12-10 17:31:50 +0100

    added omments and readme

--- src/test/kotlin/SimpleTest.kt
+++ src/test/kotlin/SimpleTest.kt
@@ -1,6 +1,9 @@
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+// The class below causes inner compiler error "Validation failed in file StreamingJsonDecoder.kt"
+// Comment this class out to avoid it:
+@Suppress("unused")
 class MyApi<T: WithAdapter>: CommandHost<T>() {
     val foo by command<String,String>()
 }
